75211a3a4c72f021861fb5ce868ab511dd6f91dc
SGBooky <bbschlicting@gmail.com>
2026-03-07 02:58:18 -0700

    Update scraping logic to include additional URLs and extract prices from listings

--- src/scrape.js
+++ src/scrape.js
@@ -40,21 +40,32 @@ export async function scrapeTCGPlayer() {
   // );
 
   const urlPages = [
-    "https://www.tcgplayer.com/product/42346/pokemon-base-set-alakazam?page=1&Language=English&Condition=Near+Mint",
-    "https://www.tcgplayer.com/product/91146?Language=English&Condition=Near+Mint&page=1",
+    "https://www.tcgplayer.com/product/236377/pokemon-swsh05-battle-styles-v-strikers-tin-tyranitar-v?Language=English&page=1",
+    // "https://www.tcgplayer.com/product/42346/pokemon-base-set-alakazam?page=1&Language=English&Condition=Near+Mint",
+    // "https://www.tcgplayer.com/product/91146?Language=English&Condition=Near+Mint&page=1",
+    // "https://www.tcgplayer.com/product/90113/pokemon-neo-discovery-tyranitar-12?Language=English&Condition=Near+Mint&page=1",
+    // "https://www.tcgplayer.com/product/241764/pokemon-swsh06-chilling-reign-weezing?Language=English&Condition=Near+Mint&page=1",
+    // "https://www.tcgplayer.com/product/241765/pokemon-swsh06-chilling-reign-crobat?Language=English&Condition=Near+Mint&page=1",
+    // "https://www.tcgplayer.com/product/241766/pokemon-swsh06-chilling-reign-sableye?Language=English&Condition=Near+Mint&page=1",
+    // "https://www.tcgplayer.com/product/241767/pokemon-swsh06-chilling-reign-gengar?Language=English&Condition=Near+Mint&page=1",
+    // "https://www.tcgplayer.com/product/241768/pokemon-swsh06-chilling-reign-mimikyu?Language=English&Condition=Near+Mint&page=1",
+    // "https://www.tcgplayer.com/product/241769/pokemon-swsh06-chilling-reign-dusknoir?Language=English&Condition=Near+Mint&page=1",
   ];
 
   for (const url of urlPages) {
     await page.goto(url, { waitUntil: "networkidle" });
+
     // Wait for listings to appear
     await page.waitForSelector(".listing-item__listing-data__info__price", 
       { timeout: 10000 }
     );
+
     // Extract all prices
     const prices = await page.$$eval(
       ".listing-item__listing-data__info__price",
       elements => elements.map(element => element.textContent.trim())
     );
+
     // PRICES PRINTED HERE
     console.log("Prices:", prices);
 
